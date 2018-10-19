8ed2dd3730003df687254f8b4f9b6e7b064411d3
Neil Puttick <m@idst.org>
2018-10-19 16:59:03 +0100

    footer submenu added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+
 
 const Footer = () => (
   <StaticQuery
@@ -13,6 +12,12 @@ const Footer = () => (
             object_slug
             url
             wordpress_id
+            wordpress_children {
+              wordpress_id
+              title
+              url
+              object_slug
+            }
           }
         }
         allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
@@ -29,21 +34,32 @@ const Footer = () => (
     render={data => (
       <nav className="navbar is-transparent">
       <div className="container">
-        <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-              <figure className="image">
-                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-              </figure>
-            </Link>
-          </div>
-          <div className="navbar-start">
-        {data.wordpressWpApiMenusMenusItems.items.map(item => (
-          <Link key={`/${item.wordpress_id}`} to={`/${item.object_slug}`} style={{
+        
+      <div className="navbar-start">
+        <ul>
+                {data.wordpressWpApiMenusMenusItems.items.map((item) =>
+                    <li key={`/${item.wordpress_id}`}>
+                        <Link to={`/${item.object_slug}`} style={{
             color: '#000',
             marginRight: '2em',
-          }}>  {item.title} </Link>
-        ))}
+          }}>
+                            {item.title}
+                        </Link>
+                        <ul>
+                            {item.wordpress_children && item.wordpress_children.map((subitem) =>
+                                <li key={item.wordpress_id}>
+                                    <Link to={subitem.object_slug}>
+                                        {subitem.title}
+                                    </Link>
+                                </li>
+                            )}
+                        </ul>
+                    </li>
+                )}
+            </ul>  
+
         </div>
+
         </div>
       </nav>
     )}
