8a5c259943e32d34ac8fbdb341cc83a96671d7a6
Neil Puttick <m@idst.org>
2018-10-22 18:54:44 +0100

    stupid

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,7 @@ import TimeUntil from './TimeUntil'
 import PartnerCarousel from './PartnerCarousel'
 import SupporterCarousel from './SupporterCarousel'
 import Footer from '../components/Footer'
+import BackTop from '../components/BackToTop'
 import './all.sass'
 
 import config from "../../data/SiteConfig";
@@ -23,6 +24,7 @@ const TemplateWrapper = ({ children }) => (
     <PartnerCarousel/>
     <SupporterCarousel/>
     <Footer />
+    <BackTop />
   </div>
 )
 
