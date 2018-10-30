32cc94db622eb2a688efc2052ac9e0df4ac23d4c
Neil Puttick <m@idst.org>
2018-10-30 21:20:37 +0000

    Removed extra carousels

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,14 +2,8 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from '../Top/Navbar'
-import TopNavbar from '../Top/TopNavbar'
-import FrontCarousel from '../Carousels/FrontCarousel'
-import TimeUntil from '../Top/TimeUntil'
-import PartnerCarousel from '../Carousels/PartnerCarousel'
-import SupporterCarousel from '../Carousels/SupporterCarousel'
 import Footer from '../Footer/Footer'
 import ScrollButton from '../utilities/ScrollToTop'
-
 
 import config from "../../../data/SiteConfig";
 
@@ -17,9 +11,6 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title={`${config.siteTitle} | ${config.siteDescription}`} />
     <Navbar />
-    <PartnerCarousel />
-    <SupporterCarousel />
-    <FrontCarousel />
     <div>{children}</div>
     <Footer/>
     <ScrollButton scrollStepInPx="50" delayInMs="16.66"/>
