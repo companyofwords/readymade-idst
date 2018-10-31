0a18746f560d218f9b277e3971838a89d470c17e
Neil Puttick <m@idst.org>
2018-10-31 16:58:39 +0000

    tag styling

--- src/components/Index/TagList.js
+++ src/components/Index/TagList.js
@@ -3,6 +3,45 @@ import { Link, StaticQuery, graphql } from 'gatsby'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { FacebookProvider, Like } from 'react-facebook'
+import Chip from '@material-ui/core/Chip'
+
+import { css } from 'emotion'
+
+
+const chipstyle = css`
+`
+
+const fbooklikes = css`
+`
+
+const paragraph = css`
+          padding: 0 20px 20px;
+          max-width: 240px;
+
+        h4 {
+          font-family: "Permanent Marker";
+          color: white;
+          font-size: 1.8em;
+        }
+        
+        .${fbooklikes} {
+          padding:20px;
+          margin-top: 20px;
+          border-top: 2px white solid;
+          color: white;
+          overflow : auto;
+          display: inline-block;
+
+          & span {
+            color: white;
+          }
+        }
+        .${chipstyle} {
+          margin-right: 5px;
+          margin-bottom: 5px;
+        }
+      
+        `
 
 const TagList = () => (
   <StaticQuery
@@ -32,39 +71,43 @@ const TagList = () => (
     `}
     render={
       data => (
-      <div>
+        
+      <div className={paragraph}>
       
         
       <h4>Tags</h4>
                 {data.allWordpressTag.edges.map(({ node: tag }) =>
-                    <div key={tag.id}>
-                       
-                       <span><Link to={`/tags/${tag.slug}/`} dangerouslySetInnerHTML={{ __html: tag.name }}> 
-                                </Link></span>
-      
-                                
+                    <span key={tag.id}>
+
+                                <Chip 
+        className={chipstyle}
+        label={tag.name}
+        component="a"
+        href={`/tags/${tag.slug}/`}
+        clickable
+      />            
 
       
-      </div>
+      </span>
                 )}
       <h4>Categories</h4>
 
       {data.allWordpressCategory.edges.map(({ node: tag }) =>
-                    <div key={tag.id}>
-                       
-                    <span><Link to={`/tags/${tag.slug}/`} dangerouslySetInnerHTML={{ __html: tag.name }}> 
-                                </Link></span>
-      
-                                
-
-      
-      </div>
+                    <span key={tag.id}>
+      <Chip 
+        className={chipstyle}
+        label={tag.name}
+        component="a"
+        href={`/tags/${tag.slug}/`}
+        clickable
+      />  
+      </span>
                 )}
-             
-      
+      <div className={fbooklikes}>
       <FacebookProvider appId="555701548185468">
-      <Like href="http://www.facebook.com/inourmidsts" colorScheme="dark" showFaces share />
+      <Like href="http://www.facebook.com/inourmidsts" colorScheme="dark" showFaces shares width="140px"/>
         </FacebookProvider>
+        </div>           
       </div>
     )}
   />
