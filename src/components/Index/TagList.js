c92fdb7f6dc34e94d3f27b9b667b3c0fc00f8449
Neil Puttick <m@idst.org>
2018-10-31 13:02:27 +0000

    Added Tag and Category List

--- src/components/Index/TagList.js
+++ src/components/Index/TagList.js
@@ -4,105 +4,67 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { FacebookProvider, Like } from 'react-facebook'
 
-
-import { css } from 'emotion'
-
 const TagList = () => (
   <StaticQuery
     query={graphql`
       query {
-        wordpressAcfOptions {
-          id
-          wordpress_id
-          options {
-            logo {
-              id
-            }
-            title
-            sitesubtitle
-            sitemaintitle
-            sitedescription
-            sitelongread
-            backuptoptext
-            allrightsreserved
-            copyright
-            donatetext
-            donatelink
-            weneed {
-              item
-              itemnumber
-            }
-            logo {
-              localFile {
-                childImageSharp {
-                  resize(width: 180, height: 180) {
-                    src
-                  }
-                }
+        allWordpressTag {
+            edges {
+              node {
+                id
+                name
+                link
+                slug
               }
             }
           }
-        }
-        wordpressWpApiMenusMenusItems(slug: { eq: "main-nav" }) {
-          items {
-            title
-            object_slug
-            url
-            wordpress_id
-            wordpress_children {
-              wordpress_id
-              title
-              url
-              object_slug
-            }
-          }
-        }
-        allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
+         allWordpressCategory {
           edges {
             node {
-              title
+              id
+              name
+              link
               slug
-              
             }
           }
-        }
       }
+    }
     `}
-    render={data => (
-      <div
-      className={css`
-      background: #373142;
-      color: white;
-      padding: 10px;
-    `}>
+    render={
+      data => (
+      <div>
+      
         
-        <Link to="/">
-              <figure>
-                <img src={data.wordpressAcfOptions.options.logo.localFile.childImageSharp.resize.src} alt={data.wordpressAcfOptions.options.title} style={{ width: '88px' }} />
-                
-              </figure>
-            </Link>
-            <h2>{data.wordpressAcfOptions.options.sitesubtitle}</h2>
-            <p>{data.wordpressAcfOptions.options.sitedescription}</p>
-            <Link to={`${data.wordpressAcfOptions.options.donatelink}`}>
-            <p>{data.wordpressAcfOptions.options.donatetext}</p>
-            </Link>
-            
-            
-          
-            {data.wordpressAcfOptions.options.weneed && data.wordpressAcfOptions.options.weneed.length ? (
-                <div>
-                  <h4>We Need:</h4>
-                  <ul className="taglist">
-                    {data.wordpressAcfOptions.options.weneed.map(weneed => (
-                      <li key={weneed.itemnumber}>
-                      <p>{weneed.item}</p>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-        
+      <h4>Tags</h4>
+                {data.allWordpressTag.edges.map(({ node: tag }) =>
+                    <div key={tag.id}>
+                       
+                       <span><Link to={`/tags/${tag.slug}/`} dangerouslySetInnerHTML={{ __html: tag.name }}> 
+                                </Link></span>
+      
+                                
+
+      
+      </div>
+                )}
+      <h4>Categories</h4>
+
+      {data.allWordpressCategory.edges.map(({ node: tag }) =>
+                    <div key={tag.id}>
+                       
+                    <span><Link to={`/tags/${tag.slug}/`} dangerouslySetInnerHTML={{ __html: tag.name }}> 
+                                </Link></span>
+      
+                                
+
+      
+      </div>
+                )}
+             
+      
+      <FacebookProvider appId="555701548185468">
+      <Like href="http://www.facebook.com/inourmidsts" colorScheme="dark" showFaces share />
+        </FacebookProvider>
       </div>
     )}
   />
